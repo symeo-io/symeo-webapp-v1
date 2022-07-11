dabf3f6959526e412ee68f6d8e0cb208ce1088a6
Georges Biaux <georges.biaux@gadz.org>
2022-07-11 08:33:29 +0200

    Set up deployment with circleci

---
+++ infrastructure/deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+source ./utils.sh
+
+set -e
+
+########################
+### NAMING ARGUMENTS ###
+########################
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -r|--region)
+    REGION="$2"
+    shift # past argument
+    ;;
+    -e|--env)
+    ENV="$2"
+    shift # past argument
+    ;;
+    -p|--profile)
+    PROFILE="$2"
+    shift # past argument
+    ;;
+    *)
+    printf "***************************\n"
+    printf "* Error: Invalid argument.*\n"
+    printf "***************************\n"
+    exit 1
+esac
+shift # past argument or value
+done
+
+# We check if AWS Cli profile is in parameters to set env var
+if [ -z "${PROFILE}" ]
+then
+echo "Profile parameter is empty, the default profile will be used!"
+else
+    export AWS_PROFILE=${PROFILE}
+fi
+
+export_stack_outputs splane-front-s3-${ENV} ${REGION}
+
+yarn build:${ENV}
+
+aws s3 cp ../build s3://${FrontS3Bucket} --recursive
+
+echo "DONE"
