c2a17c8ef56462d942c4a27b2bd4d07148eb3c1a
Georges Biaux <georges.biaux@gadz.org>
2022-07-12 10:16:56 +0200

    Implement auth0 authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Auth0Provider} from "@auth0/auth0-react";
+import Home from "./components/pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
+      redirectUri={window.location.origin}
+    >
+      <Home />
+    </Auth0Provider>
   );
 }
 
