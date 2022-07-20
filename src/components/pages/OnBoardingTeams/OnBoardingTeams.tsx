b2b9cdd0a3c4f3ed103ce92351c5dc216f3e194a
Georges Biaux <georges.biaux@gadz.org>
2022-07-20 09:44:54 +0200

    Implement add another team

--- src/components/pages/OnBoardingTeams/OnBoardingTeams.tsx
+++ src/components/pages/OnBoardingTeams/OnBoardingTeams.tsx
@@ -1,14 +1,17 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 import { useIntl } from "react-intl";
 import { useGetCurrentUserQuery } from "redux/api/user/user.api";
 import routes from "routing";
 import { useNavigate } from "react-router-dom";
-import OnBoardingPageContainer from "../../molecules/OnBoardingPageContainer/OnBoardingPageContainer";
-import OnBoardingCard from "../../molecules/OnBoardingCard/OnBoardingCard";
+import OnBoardingPageContainer from "components/molecules/OnBoardingPageContainer/OnBoardingPageContainer";
+import OnBoardingCard from "components/molecules/OnBoardingCard/OnBoardingCard";
 import CreateTeamForm, {
   CreateTeamFormValues,
-} from "../../organisms/CreateTeamForm/CreateTeamForm";
+} from "components/organisms/CreateTeamForm/CreateTeamForm";
+import Button from "components/atoms/Button/Button";
+import AddIcon from "@mui/icons-material/Add";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function OnBoardingTeams() {
   const { formatMessage } = useIntl();
@@ -27,6 +30,29 @@ function OnBoardingTeams() {
       const newTeams = [...teams];
       newTeams[index] = values;
       setTeams(newTeams);
+    },
+    [teams]
+  );
+
+  const addTeam = useCallback(
+    () =>
+      setTeams([
+        ...teams,
+        {
+          name: "",
+          repositories: [],
+        },
+      ]),
+    [teams]
+  );
+
+  const removeTeam = useCallback(
+    (index: number) => {
+      if (teams.length > 1) {
+        const newTeams = [...teams];
+        newTeams.splice(index, 1);
+        setTeams(newTeams);
+      }
     },
     [teams]
   );
@@ -50,14 +76,51 @@ function OnBoardingTeams() {
       >
         <Box sx={{ width: "100%" }}>
           {teams.map((team, index) => (
-            <CreateTeamForm
-              key={index}
-              values={team}
-              setValues={(values: CreateTeamFormValues) =>
-                setTeam(index, values)
-              }
-            />
+            <>
+              {index !== 0 && (
+                <Divider sx={{ marginTop: (theme) => theme.spacing(4) }} />
+              )}
+              <Box
+                sx={{
+                  padding: (theme) => theme.spacing(1),
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <ClearIcon
+                  onClick={() => removeTeam(index)}
+                  sx={{
+                    cursor: "pointer",
+                    fontSize: "18px",
+                    visibility: teams.length <= 1 ? "hidden" : undefined,
+                  }}
+                />
+              </Box>
+              <CreateTeamForm
+                key={index}
+                values={team}
+                setValues={(values: CreateTeamFormValues) =>
+                  setTeam(index, values)
+                }
+                sx={{
+                  marginTop:
+                    index !== 0 ? (theme) => theme.spacing(3) : undefined,
+                }}
+              />
+            </>
           ))}
+          <Box
+            sx={{
+              textAlign: "center",
+              marginTop: (theme) => theme.spacing(8),
+            }}
+          >
+            <Button startIcon={<AddIcon />} onClick={addTeam}>
+              {formatMessage({
+                id: "on-boarding.create-teams.add-team-button-label",
+              })}
+            </Button>
+          </Box>
         </Box>
       </OnBoardingCard>
     </OnBoardingPageContainer>
