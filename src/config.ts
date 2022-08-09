caf5afef82534b4f3c98f6b0f904ebb8c8bc09e6
Georges Biaux <georges.biaux@gadz.org>
2022-08-09 15:57:13 +0200

    remove sentry

--- src/config.ts
+++ src/config.ts
@@ -11,9 +11,6 @@ export const config = {
   githubApp: {
     name: process.env.REACT_APP_GITHUB_APP_NAME ?? "",
   },
-  sentry: {
-    dsn: process.env.REACT_APP_SENTRY_DSN ?? "",
-  },
   datadog: {
     applicationId: process.env.REACT_APP_DATADOG_APPLICATION_ID ?? "",
     clientToken: process.env.REACT_APP_DATADOG_CLIENT_TOKEN ?? "",
--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,6 @@ import { theme } from "theme/theme";
 import { ThemeProvider } from "@mui/material";
 import { RawIntlProvider } from "react-intl";
 import { intl } from "intl";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import ConfirmDialogProvider from "providers/confirm/ConfirmDialogProvider";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import dayjs from "dayjs";
@@ -24,15 +22,6 @@ import { datadogRum } from "@datadog/browser-rum";
 import { datadogLogs } from "@datadog/browser-logs";
 
 dayjs.extend(customParseFormat);
-
-if (config.sentry.dsn) {
-  Sentry.init({
-    dsn: config.sentry.dsn,
-    environment: config.env,
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
-}
 
 if (config.datadog.clientToken) {
   datadogRum.init({
