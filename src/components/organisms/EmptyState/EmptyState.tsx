2039e155af30798cd8d967488c2f442c5068c494
Georges Biaux <georges.biaux@gadz.org>
2022-09-13 09:44:25 +0200

    Add empty state for team goals

---
+++ src/components/organisms/EmptyState/EmptyState.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { PropsWithSx } from "types/PropsWithSx";
+import Button, { ButtonProps } from "components/atoms/Button/Button";
+
+export type EmptyStateProps = PropsWithSx & {
+  title: string;
+  message: string;
+  button: {
+    label: string;
+    onClick: ButtonProps["onClick"];
+  };
+};
+
+function EmptyState({ title, message, button, sx }: EmptyStateProps) {
+  return (
+    <Box sx={{ textAlign: "center", ...sx }}>
+      <Typography component="div" variant="h2">
+        {title}
+      </Typography>
+      <Typography
+        variant="body2"
+        color="secondary"
+        sx={{ marginTop: (theme) => theme.spacing(1) }}
+      >
+        {message}
+      </Typography>
+      <Box sx={{ marginTop: (theme) => theme.spacing(3) }}>
+        <Button variant="contained" color="primary" onClick={button.onClick}>
+          {button.label}
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default EmptyState;
--- src/components/pages/TeamGoals/TeamGoals.tsx
+++ src/components/pages/TeamGoals/TeamGoals.tsx
@@ -6,8 +6,11 @@ import DateRangeSelector from "components/molecules/DateRangeSelector/DateRangeS
 import TeamGoalDashboardPanel from "components/organisms/TeamGoalDashboardPanel/TeamGoalDashboardPanel";
 import { standards } from "constants/standards";
 import React from "react";
+import EmptyState from "components/organisms/EmptyState/EmptyState";
+import { useNavigate } from "hooks/useNavigate";
 
 function TeamGoals() {
+  const navigate = useNavigate();
   const { formatMessage } = useIntl();
   const { selectedTeam, goals } = useCurrentUser();
   const { isLoading: isLoadingProcessingInitialJob } = useDataStatus();
@@ -34,27 +37,54 @@ function TeamGoals() {
         </Typography>
         <DateRangeSelector />
       </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          marginTop: (theme) => theme.spacing(2),
-          marginBottom: (theme) => theme.spacing(6),
-        }}
-      >
-        {!isLoadingProcessingInitialJob &&
-          goals?.map((goal, index) => (
-            <TeamGoalDashboardPanel
-              key={goal.id}
+      {!isLoadingProcessingInitialJob && (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              marginTop: (theme) => theme.spacing(2),
+              marginBottom: (theme) => theme.spacing(6),
+            }}
+          >
+            {goals?.map((goal) => (
+              <TeamGoalDashboardPanel
+                key={goal.id}
+                sx={{
+                  margin: (theme) => theme.spacing(1),
+                  width: "280px",
+                }}
+                standard={standards[goal.standard_code]}
+                goal={goal}
+              />
+            ))}
+          </Box>
+          {(!goals || goals.length === 0) && (
+            <Box
               sx={{
-                margin: (theme) => theme.spacing(1),
-                width: "280px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                paddingY: (theme) => theme.spacing(8),
+                paddingX: (theme) => theme.spacing(1),
               }}
-              standard={standards[goal.standard_code]}
-              goal={goal}
-            />
-          ))}
-      </Box>
+            >
+              <EmptyState
+                title={formatMessage({ id: "team-goals.empty-state.title" })}
+                message={formatMessage({
+                  id: "team-goals.empty-state.message",
+                })}
+                button={{
+                  label: formatMessage({
+                    id: "team-goals.empty-state.button-label",
+                  }),
+                  onClick: () => navigate("teamGoalsLibrary"),
+                }}
+              />
+            </Box>
+          )}
+        </>
+      )}
     </Box>
   );
 }
