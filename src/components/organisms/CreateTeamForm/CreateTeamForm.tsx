f5541d3e09e48062cb3e8c1f25d7161d291f5072
Georges Biaux <georges.biaux@symeo.io>
2022-10-20 11:09:40 +0200

    Add select all to team form

--- src/components/organisms/CreateTeamForm/CreateTeamForm.tsx
+++ src/components/organisms/CreateTeamForm/CreateTeamForm.tsx
@@ -1,5 +1,13 @@
 import React, { PropsWithChildren, useCallback, useMemo } from "react";
-import { Autocomplete, Box, Chip, InputAdornment } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  Checkbox,
+  Chip,
+  Divider,
+  InputAdornment,
+  Popper,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "components/molecules/TextField/TextField";
 import { useIntl } from "react-intl";
@@ -8,6 +16,9 @@ import { Repository } from "redux/api/repositories/repositories.types";
 import GitHubIcon from "./GitHubIcon";
 import { FormErrors } from "./utils";
 import { PropsWithSx } from "types/PropsWithSx";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import IndeterminateCheckBoxIcon from "@mui/icons-material/IndeterminateCheckBox";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
 export type EditOrCreateTeamFormValues = {
   name: string;
@@ -21,6 +32,10 @@ export type OnBoardingCardProps = PropsWithChildren &
     errors: FormErrors<EditOrCreateTeamFormValues>;
     setErrors: (errors: FormErrors<EditOrCreateTeamFormValues>) => void;
   };
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+const intermediateIcon = <IndeterminateCheckBoxIcon fontSize="small" />;
 
 function CreateTeamForm({
   values,
@@ -61,6 +76,38 @@ function CreateTeamForm({
     [repositories, values.repositoryIds]
   );
 
+  const allSelected = useMemo(
+    () => values.repositoryIds.length === repositories.length,
+    [repositories.length, values.repositoryIds.length]
+  );
+
+  const selectionEmpty = useMemo(
+    () => values.repositoryIds.length === 0,
+    [values.repositoryIds.length]
+  );
+
+  const selectAll = useCallback(() => {
+    setValues({
+      ...values,
+      repositoryIds: repositories.map((repo) => repo.id),
+    });
+  }, [repositories, setValues, values]);
+
+  const deselectAll = useCallback(() => {
+    setValues({
+      ...values,
+      repositoryIds: [],
+    });
+  }, [setValues, values]);
+
+  const handleSelectAllChange = useCallback(() => {
+    if (allSelected || !selectionEmpty) {
+      return deselectAll();
+    }
+
+    return selectAll();
+  }, [allSelected, deselectAll, selectAll, selectionEmpty]);
+
   return (
     <Box sx={{ ...sx }}>
       <TextField
@@ -89,6 +136,7 @@ function CreateTeamForm({
         value={selectedRepositories}
         onChange={handleRepositoriesChange}
         multiple
+        disableCloseOnSelect
         options={repositories}
         getOptionLabel={(option) => option.name}
         noOptionsText={formatMessage({
@@ -98,6 +146,58 @@ function CreateTeamForm({
         loadingText={formatMessage({
           id: "on-boarding.create-teams.form.repositories-field-loading",
         })}
+        PopperComponent={(param) => (
+          <Popper {...param}>
+            <Box
+              sx={{
+                overflow: "hidden",
+                background: "white",
+                borderRadius: "4px",
+                boxShadow:
+                  "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+              }}
+            >
+              <Box
+                sx={{
+                  padding: "6px 16px",
+                  cursor: "pointer",
+                  fontSize: "14px",
+                  color: "rgba(0,0,0,0.87)",
+                  fontWeight: 100,
+                  textTransform: "uppercase",
+                }}
+                onMouseDown={(e) => e.preventDefault()}
+                onClick={handleSelectAllChange}
+              >
+                <Checkbox
+                  checked={!selectionEmpty}
+                  onChange={handleSelectAllChange}
+                  checkedIcon={allSelected ? checkedIcon : intermediateIcon}
+                  icon={icon}
+                  id="check-all"
+                  sx={{ marginRight: "8px" }}
+                  onMouseDown={(e) => e.preventDefault()}
+                />
+                {formatMessage({
+                  id: selectionEmpty
+                    ? "on-boarding.create-teams.form.select-all-label"
+                    : "on-boarding.create-teams.form.deselect-all-label",
+                })}
+              </Box>
+              <Divider />
+              <Box
+                sx={{
+                  "& .MuiPaper-root": {
+                    boxShadow: "none",
+                    borderRadius: 0,
+                  },
+                }}
+              >
+                {param.children as React.ReactNode}
+              </Box>
+            </Box>
+          </Popper>
+        )}
         renderTags={(value: readonly Repository[], getTagProps) =>
           value.map((option: Repository, index: number) => (
             <Chip
@@ -107,6 +207,17 @@ function CreateTeamForm({
             />
           ))
         }
+        renderOption={(props, option, { selected }) => (
+          <li {...props}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option.name}
+          </li>
+        )}
         renderInput={(params) => (
           <TextField
             {...params}
